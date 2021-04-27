f25080354848f78bc8e6c3216e2df29ca0ec954e
jarongrigat <jaron.grigat@googlemail.com>
2021-04-27 16:52:18 +0200

    Changed format to work with newest doberman version

--- alicat_mcv.py
+++ alicat_mcv.py
@@ -1,21 +1,18 @@
 from Doberman import LANSensor, utils
 import re  # EVERYBODY STAND BACK xkcd.com/208
 
+
 class alicat_mcv(LANSensor):
-
     """
-    Flow controller
+    Alicat MCV flow controller
     """
 
-    def SetParameters(self):
+    def set_parameters(self):
         self._msg_end = '\r'
 
-
-    def ProcessOneReading(self, name, data):
-        ret = []
-        pattern = re.compile((f'{utils.number_regex}').encode())
+    def process_one_reading(self, name, data):
+        pattern = re.compile(f'{utils.number_regex}'.encode())
         matches = re.findall(pattern, data)
         ret = [float(match) for match in matches][:5]
         if len(ret) == 5:
             return ret
-
--- lmbox_pancake.py
+++ lmbox_pancake.py
@@ -1,26 +1,24 @@
 from Doberman import LANSensor
 import struct
 import time
+import socket
+
 
 class lmbox_pancake(LANSensor):
     """
     Custom level meter box for pancake. The device is read out through an RS485 to ETH adapter, that's why
-    it inherits from LANSensor. send_recv() is modified to sleep loger since the device reacts slower than
-    a standart LAN sensor.
+    it inherits from LANSensor. send_recv() is modified to sleep longer since the device reacts slower than
+    a standard LAN sensor.
     """
-    
-    def SetParameters(self):
-        self._msg_start = ''
-        self._msg_end = ''
 
+    def set_parameters(self):
         self.eol = b'\r'
         self.split = b'\x06'
 
-    def ProcessOneReading(self, name, data):
+    def process_one_reading(self, name, data):
         """
         Data structure: 6 times 4 integers divided by the split character plus the EOL-character.
         """
-
         if self.eol in data:
             data = data.split(self.eol)[0]
         else:
@@ -32,36 +30,32 @@ class lmbox_pancake(LANSensor):
         if len(data) != 54:
             self.logger.debug(f'salvaging unsuccessful, {data}')
             return
-        decoded = struct.unpack('<'+6*(4*'h'+'c'), data)
+        decoded = struct.unpack('<' + 6 * (4 * 'h' + 'c'), data)
         c_meas = []
         for i in range(6):
-            lm_values = decoded[5*i:5*i+4]
+            lm_values = decoded[5 * i:5 * i + 4]
             offset_values = sorted(lm_values)[:2]
             n_off = sum(offset_values)
             index_min = lm_values.index(min(offset_values))
-            index = (index_min+2)%4 if lm_values[(index_min+1)%4] in offset_values else (index_min+1)%4
+            index = (index_min + 2) % 4 if lm_values[(index_min + 1) % 4] in offset_values else (index_min + 1) % 4
             n_ref = lm_values[index]
-            n_x = lm_values[(index+1)%4]
-            c_meas.append(self.c_ref[i]*(n_x-n_off)/(n_ref-n_off))
+            n_x = lm_values[(index + 1) % 4]
+            c_meas.append(self.c_ref[i] * (n_x - n_off) / (n_ref - n_off))
         return c_meas
-    
 
     def salvage_input(self, data):
         """
         This may or may not salvage some reading if the length of the data doesn't fit the normal 
         structure.
         """
-
         place_holder = b'\x02\x00\x02\x00\x05\x00\x03\x00'
         data_temp = data.split(self.split)[:-1]
-        data_list = [p if len(p)==8 else place_holder for p in data_temp]
-        data=self.split.join(data_list) + self.split
-        return(data)
-    
+        data_list = [p if len(p) == 8 else place_holder for p in data_temp]
+        data = self.split.join(data_list) + self.split
+        return data
 
-    def SendRecv(self, message):
+    def send_recv(self, message):
         ret = {'retcode': 0, 'data': None}
-
         if not self._connected:
             self.logger.error(f'No sensor connected, can\'t send message {message}')
             ret['retcode'] = -1
@@ -75,7 +69,6 @@ class lmbox_pancake(LANSensor):
             ret['retcode'] = -2
             return ret
         time.sleep(1)
-
         try:
             ret['data'] = self._device.recv(1024)
         except socket.error as e:
