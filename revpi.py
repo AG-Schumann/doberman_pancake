ce9fa444b877e5891c3a59e6a5b89b7f8c899062
jarongrigat <jaron.grigat@googlemail.com>
2022-01-18 14:28:13 +0100

    reading/writing by variable name

--- revpi.py
+++ revpi.py
@@ -1,3 +1,6 @@
+import fcntl
+
+import struct
 from Doberman import Sensor, utils
 import re
 
@@ -20,6 +23,62 @@ class revpi(Sensor):
              lambda x: self.commands['writeOutput'].format(**x.groupdict())),
         ]
 
+    def get_position(self, name):
+        prm = (b'K'[0] << 8) + 17
+        struct_name = struct.pack('37s', name.encode())
+        with open('/dev/piControl0', 'wb+', 0) as f:
+            ret = fcntl.ioctl(f, prm, struct_name)
+        return ret
+
+    def set_value(self, name, value):
+        """
+        Set the value of a variable (most likely an output)
+        :param name: name of the variable as defined in the Pictory
+        :param value: value to be set
+        """
+        pos = self.get_position(name)
+        offset = struct.unpack_from('>H', pos, 32)[0]
+        length = struct.unpack_from('>H', pos, 35)[0]
+        prm = (b'K'[0] << 8) + 16
+        value = bytearray([0, 0, 0, 0])
+        if length == 1:  # single bit
+            if value in [0,1]:
+                bit = struct.unpack_from('B', pos, 34)[0]
+                struct.pack_into('>H', value, 0, offset)
+                struct.pack_into('B', value, 2, bit)
+                struct.pack_into('B', value, 3, value)
+                with open('/dev/piControl0', 'wb+', 0) as f:
+                    fcntl.ioctl(f, prm, value)
+            else:
+                self.logger.debug(f'Invalid value {value}. choose 0 or 1')
+        else:  # writing 2 bytes
+            with open('/dev/piControl0', 'wb+', 0) as f:
+                f.seek(int(offset >> 8))
+                f.write(int(value).to_bytes(2, 'little'))
+
+    def read(self, name):
+        """
+        Read value of a variable
+        :param name: name of the variable as defined in the Pictory
+        """
+        pos = self.get_position(name)
+        value = bytearray([0, 0, 0, 0])
+        offset = struct.unpack_from('>H', pos, 32)[0]
+        length = struct.unpack_from('>H', pos, 35)[0]
+        prm = (b'K'[0] << 8) + 15
+        if length == 1: # single bit
+            bit = struct.unpack_from('B', pos, 34)[0]
+            struct.pack_into('>H', value, 0, offset)
+            struct.pack_into('B', value, 2, bit)
+            with open('/dev/piControl0', 'wb+', 0) as f:
+                fcntl.ioctl(f, prm, value)
+                ret = value[3]
+        else:  # two bytes
+            with open('/dev/piControl0', 'wb+', 0) as f:
+                f.seek(int(offset >> 8))
+                ret = int.from_bytes(f.read(2), 'little')
+        return ret
+
     def execute_command(self, command):
         # command looks like "set <target> <value>"
         # this is wrapped in a try-except one call up so we don't
@@ -31,7 +90,7 @@ class revpi(Sensor):
             module = 3
             ch = 13
             value = 1 if value == 'open' else 0
-        return self.commands['writeOutput'].format(module=module, ch=output, value=value)
+        return self.commands['writeOutput'].format(module=module, ch=ch, value=value)
 
     def send_recv(self, message):
         self.bytes_per_module = 89
