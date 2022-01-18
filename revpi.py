f0de20411e4397850cfa24605a9598194c35a9c9
jarongrigat <jaron.grigat@googlemail.com>
2022-01-18 17:07:58 +0100

    ready for testing

--- revpi.py
+++ revpi.py
@@ -1,7 +1,6 @@
 import fcntl
-
 import struct
-from Doberman import Sensor, utils
+from Doberman import Sensor
 import re
 
 
@@ -12,71 +11,69 @@ class revpi(Sensor):
 
     def set_parameters(self):
         self.commands = {
-            'readInput': 'r,{module},i,{ch}',
-            'readOutput': 'r,{module},o,{ch}',
-            'readRTD': 'r,{module},r,{ch}',
-            'writeOutput': 'w,{module},o,{ch},{value}'
+            'read': 'r {name}',
+            'write': 'w {name} {value}'
         }
-        # self.reading_pattern = re.compile(('(?P<value>\d+)').encode())
         self.command_patterns = [
-            (re.compile(r'write module (?P<module>\d+) output (?P<ch>1|2): (?P<value>\d{1,5})'),
-             lambda x: self.commands['writeOutput'].format(**x.groupdict())),
+            (re.compile(r'set (?P<name>\w+) (?P<value>\d{1,5})'),
+             lambda x: self.commands['write'].format(**x.groupdict())),
         ]
+        self.positions = {}
+        self.img = open('/dev/piControl0', 'wb+', 0)
+        self.targets = {'fast_cooling_valve': 'O_13', }
+        self.keywords = {'open': 1, 'close': 0, }
 
     def get_position(self, name):
         prm = (b'K'[0] << 8) + 17
         struct_name = struct.pack('37s', name.encode())
-        with open('/dev/piControl0', 'wb+', 0) as f:
-            ret = fcntl.ioctl(f, prm, struct_name)
+        ret = fcntl.ioctl(self.img, prm, struct_name)
         return ret
 
-    def set_value(self, name, value):
+    def write(self, name, value):
         """
         Set the value of a variable (most likely an output)
         :param name: name of the variable as defined in the Pictory
         :param value: value to be set
         """
-        pos = self.get_position(name)
-        offset = struct.unpack_from('>H', pos, 32)[0]
-        length = struct.unpack_from('>H', pos, 35)[0]
+        if name not in self.positions:
+            self.positions[name] = self.get_position(name)
+        offset = struct.unpack_from('>H', self.positions[name], 32)[0]
+        length = struct.unpack_from('>H', self.positions[name], 35)[0]
         prm = (b'K'[0] << 8) + 16
-        value = bytearray([0, 0, 0, 0])
+        byte_array = bytearray([0, 0, 0, 0])
         if length == 1:  # single bit
-            if value in [0,1]:
-                bit = struct.unpack_from('B', pos, 34)[0]
-                struct.pack_into('>H', value, 0, offset)
-                struct.pack_into('B', value, 2, bit)
-                struct.pack_into('B', value, 3, value)
-                with open('/dev/piControl0', 'wb+', 0) as f:
-                    fcntl.ioctl(f, prm, value)
+            if value in [0, 1]:
+                bit = struct.unpack_from('B', self.positions[name], 34)[0]
+                struct.pack_into('>H', byte_array, 0, offset)
+                struct.pack_into('B', byte_array, 2, bit)
+                struct.pack_into('B', byte_array, 3, value)
+                fcntl.ioctl(self.img, prm, byte_array)
             else:
                 self.logger.debug(f'Invalid value {value}. choose 0 or 1')
         else:  # writing 2 bytes
-            with open('/dev/piControl0', 'wb+', 0) as f:
-                f.seek(int(offset >> 8))
-                f.write(int(value).to_bytes(2, 'little'))
+            self.img.seek(int(offset >> 8))
+            self.img.write(int(value).to_bytes(2, 'little'))
 
     def read(self, name):
         """
         Read value of a variable
         :param name: name of the variable as defined in the Pictory
         """
-        pos = self.get_position(name)
+        if name not in self.positions:
+            self.positions[name] = self.get_position(name)
         value = bytearray([0, 0, 0, 0])
-        offset = struct.unpack_from('>H', pos, 32)[0]
-        length = struct.unpack_from('>H', pos, 35)[0]
+        offset = struct.unpack_from('>H', self.positions[name], 32)[0]
+        length = struct.unpack_from('>H', self.positions[name], 35)[0]
         prm = (b'K'[0] << 8) + 15
-        if length == 1: # single bit
-            bit = struct.unpack_from('B', pos, 34)[0]
+        if length == 1:  # single bit
+            bit = struct.unpack_from('B', self.positions[name], 34)[0]
             struct.pack_into('>H', value, 0, offset)
             struct.pack_into('B', value, 2, bit)
-            with open('/dev/piControl0', 'wb+', 0) as f:
-                fcntl.ioctl(f, prm, value)
-                ret = value[3]
+            fcntl.ioctl(self.img, prm, value)
+            ret = value[3]
         else:  # two bytes
-            with open('/dev/piControl0', 'wb+', 0) as f:
-                f.seek(int(offset >> 8))
-                ret = int.from_bytes(f.read(2), 'little')
+            self.img.seek(int(offset >> 8))
+            ret = int.from_bytes(self.img.read(2), 'little')
         return ret
 
     def execute_command(self, command):
@@ -85,46 +82,26 @@ class revpi(Sensor):
         # need to do it here. We rsplit with max here so "target"
         # can potentially have spaces in it and won't cause issues
         target, value = command[4:].rsplit(' ', maxsplit=1)
-        if target == 'fast_cooling_valve':
+        if target in self.targets:
             # "fast_cooling_valve open" or something
-            module = 3
-            ch = 13
-            value = 1 if value == 'open' else 0
-        return self.commands['writeOutput'].format(module=module, ch=ch, value=value)
+            target = self.targets[target]
+            if value in self.keywords:
+                value = self.keywords[value]
+        return self.commands['write'].format(name=target, value=value)
 
     def send_recv(self, message):
-        self.bytes_per_module = 89
-        self.bytes_per_channel = 2
-        self.offset_by_type = {
-            'i': 9,
-            'o': 29,
-            'r': 21
-        }
         ret = {'retcode': 0, 'data': None}
-        msg = message.split(',')  # msg = [<r|w>, <module>, <i|o|r>, <channel>, <value>]
-        try:
-            with open('/dev/piControl0', 'wb+', 0) as f:
-                try:
-                    f.seek(int(msg[1]) * self.bytes_per_module + int(msg[3]) * self.bytes_per_channel +
-                           self.offset_by_type[msg[2]])
-                except KeyError:
-                    self.logger.error(f"Message Contains invalid type {msg[2]}. Allowed types: 'i', 'o', 'r'.")
-                    ret['retcode'] = -1
-                    return ret
-                if msg[0] == 'r':
-                    ret['data'] = int.from_bytes(f.read(self.bytes_per_channel), 'little')
-                elif msg[0] == 'w':
-                    if int(msg[4]) > (1 << 16):
-                        pass
-                    f.write(int(msg[4]).to_bytes(self.bytes_per_channel, 'little'))
-                else:
-                    self.logger.error(f"Message starts with invalid character {msg[0]}. Allowed characters: 'r' or 'w'")
-                    ret['retcode'] = -1
-                return ret
-        except Exception as e:
-            self.logger.error("Cannot open process image: %s" % type(e))
+        msg = message.split()  # msg = <r|w> <name> [<value>]
+        if msg[0] == 'r':
+            ret['data'] = self.read(msg[1])
+        elif msg[0] == 'w':
+            if int(msg[2]) > (1 << 16):
+                pass
+            self.write(msg[1], msg[2])
+        else:
+            self.logger.error(f"Message starts with invalid character {msg[0]}. Allowed characters: 'r' or 'w'")
             ret['retcode'] = -1
-            return ret
+        return ret
 
     def process_one_reading(self, name, data):
         """
