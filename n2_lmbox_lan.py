b554dd8665b373695ef03b25c4bbf368029c08c7
Adam Brown <adam.brown@physik.uni-freiburg.de>
2022-12-06 12:15:43 +0000

    Simpification and don't save mangled data

--- n2_lmbox_lan.py
+++ n2_lmbox_lan.py
@@ -19,52 +19,31 @@ class n2_lmbox_lan(LANDevice):
         """
         Data structure: 6 times 4 integers divided by the split character plus the EOL-character.
         """
-        if data[-1] == self.eol:
-            data = data[:-1]
-            if len(data) != 54:
-                self.logger.info(f'data legth is {len(data)} not 54. Trying to salvage...')
-                data = self.salvage_input(data)
-                if len(data) != 54:
-                    self.logger.info(f'salvaging unsuccessful, length is {len(data)} not 54')
-                    return
-                else:
-                    self.logger.debug('salvaging successful')
-        else:
-            self.logger.info(f'data does not end with EOL but with {data[-1]}')
-            return
-        decoded = struct.unpack('<' + 6 * (4 * 'h' + 'c'), data)
+        if data[-1] != self.eol:
+            self.logger.info(f'Data does not end with EOL but with {data[-1]}')
+        data = data.split(self.split)[:-1] # Remove EOL
+
         c_meas = []
-        for i in range(6):
+        for i, readingdata in enumerate(data):
             try:
-                lm_values = decoded[5 * i:5 * i + 4]
+                lm_values = struct.unpack('<hhhh', readingdata) # Each packet is four shorts
+
+                # The two smallest values are the offsets
+                # Order always the same, but starting value changes
                 offset_values = sorted(lm_values)[:2]
                 n_off = sum(offset_values)
                 index_min = lm_values.index(min(offset_values))
                 index = (index_min + 2) % 4 if lm_values[(index_min + 1) % 4] in offset_values else (index_min + 1) % 4
                 n_ref = lm_values[index]
                 n_x = lm_values[(index + 1) % 4]
-                # Avoid divide by zero errors
-                if n_ref == n_off:
-                    c_meas.append(None)
-                else:
-                    #self.logger.debug(f'Cap {i}, x {n_x} ref {n_ref}, n_off {n_off}')
-                    c_meas.append(self.params['c_ref'][i] * (n_x - n_off) / (n_ref - n_off))
+                if (self.params['c_ref'][i] * (n_x - n_off) / (n_ref - n_off) > 350):
+                    self.logger.debug(f"High capacitance {i}. Readings: {', '.join(lm_values)}")
+
+                c_meas.append(self.params['c_ref'][i] * (n_x - n_off) / (n_ref - n_off))
             except Exception as e:
-                self.logger.warning(f'Problem interpreting capacitance value {i}')
+                self.logger.warning(f'Problem interpreting capacitance value {i}, {e}')
                 c_meas.append(None)
         return c_meas
-
-
-    def salvage_input(self, data):
-        """
-        If a fraction of the six measurements is faulty, this makes sure the correct vones are salvaged.
-        """
-        place_holder = b'\x02\x00\x02\x00\x05\x00\x03\x00'
-        data_temp = data.split(self.split)[:-1]
-        data_list = [p if len(p) == 8 else place_holder for p in data_temp]
-        data = self.split.join(data_list) + self.split
-        return data
-
 
     def send_recv(self, message):
         ret = {'retcode': 0, 'data': None}
