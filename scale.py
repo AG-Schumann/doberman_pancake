e2937985f281096b914b8fc4949261fbbfac5561
Tiffany Luce <115090963+TiffanyLuce@users.noreply.github.com>
2023-03-01 14:41:22 +0100

    processes negative values correctls

--- scale.py
+++ scale.py
@@ -10,40 +10,10 @@ class scale(LANDevice):
     """
     eol = b'\r\x03'
 
-    def send_recv(self, message):
-        ret = {'retcode': 0, 'data': None}
-
-        if not self._connected:
-            self.logger.error(f'No device connected, can\'t send message {message}')
-            ret['retcode'] = -1
-            return ret
-        message = str(message).rstrip()
-        message = self._msg_start + message + self._msg_end
-        try:
-            self._device.sendall(message.encode())
-        except socket.error as e:
-            self.logger.fatal("Could not send message %s. Error: %s" % (message.strip(), e))
-            ret['retcode'] = -2
-            return ret
-
-        starttime = time.time()
-        try:
-            # Read until we get the end-of-line character
-            data = b''
-            for i in range(int(self.msg_wait / self.recv_interval)+1):
-                try:
-                    data += self._device.recv(self.packet_bytes)
-                except socket.timeout:
-                    continue
-                if data.endswith(self.eol):
-                    break
-            ret['data'] = re.sub('\s','',data)
-            self.logger.debug('Unprocessed data : %s' % data)
-        except socket.error as e:
-            self.logger.fatal('Could not receive data from device. Error: %s' % e)
-            ret['retcode'] = -2
-        return ret
-    
-    
-    value_pattern = re.compile((f'(?P<value>{utils.number_regex})kg').encode())
-    
+    def process_one_value(self, name=None, data=None):
+        """
+        Takes the raw data as returned by send_recv and parses
+        it for the float. Only for the scales.
+        """
+        data = re.sub('\s','',data.decode())
+        return float(re.search('(?P<value>\-?[0-9]+(?:\.[0-9]+)?)kg', data).group('value'))
