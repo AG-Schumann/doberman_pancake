f56e84da27e1a960ce34a0f5111c9c6f8c299f2c
Tiffany Luce <115090963+TiffanyLuce@users.noreply.github.com>
2023-03-01 11:08:26 +0100

    Printing none processed data as debug message
    Copy pasted the send_recv function from Doberman/DaseDevice.py for LANDevices and added the fifth line from the end : self.logger.debug('Unprocessed data : %s' % data)

--- scale.py
+++ scale.py
@@ -5,7 +5,42 @@ class scale(LANDevice):
     """
     Scale for nitrogen dewar in pancake.
     """
-
-    value_pattern = re.compile((f'(?P<value>{utils.number_regex})kg').encode())
     eol = b'\r\x03'
 
+    def send_recv(self, message):
+        ret = {'retcode': 0, 'data': None}
+
+        if not self._connected:
+            self.logger.error(f'No device connected, can\'t send message {message}')
+            ret['retcode'] = -1
+            return ret
+        message = str(message).rstrip()
+        message = self._msg_start + message + self._msg_end
+        try:
+            self._device.sendall(message.encode())
+        except socket.error as e:
+            self.logger.fatal("Could not send message %s. Error: %s" % (message.strip(), e))
+            ret['retcode'] = -2
+            return ret
+
+        starttime = time.time()
+        try:
+            # Read until we get the end-of-line character
+            data = b''
+            for i in range(int(self.msg_wait / self.recv_interval)+1):
+                try:
+                    data += self._device.recv(self.packet_bytes)
+                except socket.timeout:
+                    continue
+                if data.endswith(self.eol):
+                    break
+            ret['data'] = data
+            self.logger.debug('Unprocessed data : %s' % data)
+        except socket.error as e:
+            self.logger.fatal('Could not receive data from device. Error: %s' % e)
+            ret['retcode'] = -2
+        return ret
+    
+    
+    value_pattern = re.compile((f'(?P<value>{utils.number_regex})kg').encode())
+    
