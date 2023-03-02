4a64beb22047c945f3a3a873ad3ea6e4bf6ff5ef
Tiffany Luce <115090963+TiffanyLuce@users.noreply.github.com>
2023-03-02 10:14:27 +0100

    Negative reading of scale (#14)
    Scale has its own process_one_value function that removes spaces before finding the scale value

--- scale.py
+++ scale.py
@@ -5,7 +5,12 @@ class scale(LANDevice):
     """
     Scale for nitrogen dewar in pancake.
     """
-
-    value_pattern = re.compile((f'(?P<value>{utils.number_regex})kg').encode())
     eol = b'\r\x03'
 
+    def process_one_value(self, name=None, data=None):
+        """
+        Takes the raw data as returned by send_recv and parses
+        it for the float. Only for the scales.
+        """
+        data = re.sub('\s','',data.decode())
+        return float(re.search('(?P<value>\-?[0-9]+(?:\.[0-9]+)?)kg', data).group('value'))
