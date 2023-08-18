b07aae0dcc3e2e9969498c251fab9030316777b7
Adam Brown <adam.brown@physik.uni-freiburg.de>
2023-08-18 14:02:24 +0000

    Finally working

--- lanscale.py
+++ lanscale.py
@@ -9,8 +9,9 @@ class lanscale(LANDevice):
     """
     eol = b'\n'
     xtrem_remotep = 5555
-    msg_wait = 1.0
+    msg_wait = 1
     recv_interval = 0.01
+    max_msgs_wrong_ip = 20
     commands = {
         'tare': '0001E01020000',
         'untare': '0001E11030000',
@@ -28,12 +29,9 @@ class lanscale(LANDevice):
     def setup(self):
         self.packet_bytes = 1024
         self._device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.logger.debug('Got somewhere')
         self._device.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self._device.settimeout(self.recv_interval)
-        self.logger.debug('Got here first')
+        self._device.settimeout(0) # We will manually sleep to avoid blocking other threads
         self._device.bind(('0.0.0.0', self.xtrem_remotep))
-        self.logger.debug('Got here')
         self._msg_start = '\u0002'
         self._msg_end = '\u0003\r\n'
         return True
@@ -42,7 +40,7 @@ class lanscale(LANDevice):
         pass
 
     def execute_command(self, quantity, value):
-        return quantity
+        #return quantity
         if quantity.startswith('0001R'):
             # Allow direct command input only for read commands
             return quantity
@@ -50,12 +48,27 @@ class lanscale(LANDevice):
             # Otherwise look up from the command dictionary
             return self.commands[quantity]
 
+    def recv_or_none(self):
+        try:
+            data, fromaddress = self._device.recvfrom(self.packet_bytes)
+            return data.decode(), fromaddress
+        except BlockingIOError:
+            return None, None
+
     def send_recv(self, message):
+        sendrecvstart = time.time()
         # UDP not TCP so need a special method
         ret = {'retcode': 0, 'data': None}
 
         message = str(message).rstrip()
         wrappedmessage = self._msg_start + message + self._msg_end
+
+        # First empty the buffer so we don't get old readings
+        data = 0
+        while data is not None:
+            data, fromaddress = self.recv_or_none()
+        
+        # Now send our request for a reading
         try:
             self._device.sendto(wrappedmessage.encode(), (self.ip, self.port))
         except socket.error as e:
@@ -65,22 +78,10 @@ class lanscale(LANDevice):
 
         # Determine string we need to search for in return
         responsesearchpattern = message[2:4] + message[0:2] + message[4].lower() + message[5:9]
-        starttime = time.time()
-        try:
-            # Read until we get the end-of-line character
-            data = b''
-            for i in range(int(self.msg_wait / self.recv_interval)+1):
-                try:
-                    data, fromaddress = self._device.recvfrom(self.packet_bytes)
-                except socket.timeout:
-                    continue
-                # Got some data back but still need to check it came from the right device
-                data = data.decode()
-                if fromaddress == (self.ip, self.port) and responsesearchpattern in data:
-                    break
-            ret['data'] = data
-            self.logger.debug(f'Got {data} from device')
-        except socket.error as e:
-            self.logger.fatal(f'Could not receive data from device. Error: {e}')
-            ret['retcode'] = -2
-        return ret
+        for i in range(int(self.msg_wait / self.recv_interval) + 1):
+           data, fromaddress = self.recv_or_none()
+           if fromaddress == (self.ip, self.port) and responsesearchpattern in data:
+               ret['data'] = data
+               return ret
+           time.sleep(self.recv_interval)
+        raise IOError("Never got the expected response from the scale.")
