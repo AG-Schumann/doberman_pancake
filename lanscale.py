c7e6910ca91401cc7a6f0b82299e4e58bc25979c
Adam Brown <adam.brown@physik.uni-freiburg.de>
2023-08-18 14:03:38 +0000

    Remove strange documentation

--- lanscale.py
+++ lanscale.py
@@ -21,7 +21,7 @@ class lanscale(LANDevice):
     def process_one_value(self, name=None, data=None):
         """
         Takes the raw data as returned by send_recv and parses
-        it for the float. Only for the scales.
+        it for the float.
         """
         return float(re.search('(?P<value>\-?[0-9]+(?:\.[0-9]+)?)kg', data).group('value'))
 
