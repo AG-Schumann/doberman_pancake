d97b0dc0dd9a9429e3d97c13a8609ebb579b6327
Jaron Grigat <50399618+jarongrigat@users.noreply.github.com>
2024-10-08 16:37:36 +0200

    Caen HV plugins (#18)
    * read status of control loop
    
    * make control_loop command
    
    * values are strings
    
    * add caen HV plugins

---
+++ caen_sy5527.py
@@ -0,0 +1,32 @@
+from Doberman import  CheapSocketDevice, utils
+import re
+
+
+class caen_sy5527(CheapSocketDevice):
+    """
+    Driver for the CAEN high-voltage crate
+    """
+    def set_parameters(self):
+        self.channel_status = [
+                'on',
+                'ramp_up',
+                'ramp_down',
+                'overcurrent',
+                'overvoltage',
+                'undervoltage',
+                'external_trip',
+                'max_v',
+                'external_disable',
+                'internal_error',
+                'calibration_error',
+                'unplugged',
+                ]
+        self.command_patterns = [(re.compile('set ch(?P<ch>[1-9]?[0-9]) sl3 (?P<task>rup|rdn|tripi|tript|setp|vset|pon|pdn|pw) (?P<value>%s)' % utils.number_regex),
+            lambda m : m.group(0))]
+
+        self.value_pattern = re.compile(f'OK;(?P<value>(?:{utils.number_regex},)*{utils.number_regex})\r\n'.encode())
+
+    def process_one_reading(self, name, data):
+        self.logger.debug(data)
+        m = self.value_pattern.search(data)
+        return list(map(float, m.group('value').decode().split(',')))[0]
---
+++ caen_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $1 <install|run>"
+    exit
+fi
+
+if [[ $1 == "install" ]]; then
+    echo "Compiling.."
+    g++ -DUNIX -g -o caen_sy5527 -O2 -lcaenhvwrapper -lpthread -ldl caen_sy5527.cpp
+    if [[ $? -ne 0 ]]; then
+        echo "Do you have the appropriate libraries?"
+    else
+        echo "Success"
+    fi
+elif [[ $1 == "run" ]]; then
+    ./caen_sy5527 5000 192.168.131.12
+fi
